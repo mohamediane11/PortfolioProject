546c42d5f9fa94e589fe1f4b3f0d6deac2ee748b
mohamediane11 <122317684+mohamediane11@users.noreply.github.com>
2023-06-13 13:55:25 -0400

    Add files via upload

---
+++ Boolean arrays Practice .py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Boolean arrays
+
+# In[4]:
+
+
+import sys
+import numpy as np
+
+
+# In[5]:
+
+
+np.array([1, 2, 3, 4])
+
+
+# In[13]:
+
+
+a = np.array([1, 2, 3, 4])
+
+
+# In[14]:
+
+
+a
+
+
+# In[16]:
+
+
+a[0]
+
+
+# In[17]:
+
+
+a[[True, False, False, True]]
+
+
+# In[18]:
+
+
+a>=1
+
+
+# In[19]:
+
+
+a>=3
+
+
+# In[20]:
+
+
+a[a >=2]
+
+
+# In[21]:
+
+
+A =np.random.randint(100, size=(3, 3))
+
+
+# In[22]:
+
+
+A
+
+
+# In[23]:
+
+
+A>20
+
+
+# In[ ]:
+
+
+l = 
+
